cf3045a4962a123b6fc84450934a4827a856903a
Liu, Zhao-hui <lzhh@duck.com>
2024-01-26 16:44:26 +0800

    Update archive.sh

--- release/archive.sh
+++ release/archive.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# To use this, below things needs to be done first.
+#  * Install Ruby
+#  * Install asciidoctor-pdf with `gem install asciidoctor-pdf'
+#  * Install fonts used in theme file
+#  * Install 7-zip, or jai, to its default location
+#  * Install Git 2.36.0 or any later version
+#  * Install jg 2.10.0 or any later version
 set -o nounset
 
 archive_cleanup () {
@@ -8,8 +15,21 @@ archive_cleanup () {
 
 archive () {
     local fname
-    local exe7za="C:/Users/$USERNAME/AppData/Local/Programs/jai/7za.exe"
+    local exe7z
+    local exe_from_7zip="C:/Program Files/7-Zip/7z.exe"
+    local exe_from_jai="C:/Users/$USERNAME/AppData/Local/Programs/jai/7za.exe"
     local description toplevel
+    if test -x "$exe_from_7zip"
+    then
+        exe7z=$exe_from_7zip
+    elif test -x "$exe_from_jai"
+    then
+        exe7z=$exe_from_jai
+    else
+        >&2 printf "error: no 7-Zip excutable available\n"
+        return 1
+    fi
+    printf "using %s\n" "$exe7z"
     rm -rf tmp/ || return
     mkdir tmp || return
     trap archive_cleanup INT TERM
@@ -34,18 +54,17 @@ archive () {
         (
             cd .. || return
             tar -zcf "../$fname.tgz" "$fname" || return
-            "$exe7za" a -mx9 "../$fname.7z" "$fname" || return
+            "$exe7z" a -mx9 "../$fname.7z" "$fname" || return
         ) || return
-        "$exe7za" a -mx9 "$name.7z" * || return
+        "$exe7z" a -mx9 "$name.7z" * || return
         cp "../../$name.sfx" . || return
         cmd //c "copy /b $name.sfx + $name.7z $(cygpath -w "../../$(basename $PWD).exe")" || return
     ) || return
 }
 
 print_adoc () {
-    local asciidoctor_pdf="C:/Ruby32-x64/bin/asciidoctor-pdf"
     ls ../*.adoc 1>/dev/null 2>&1 || return 0
-    "$asciidoctor_pdf" -a scripts=cjk \
+    asciidoctor-pdf -a scripts=cjk \
         -a pdf-theme=cjk-theme.yml \
         -a pdf-fontsdir=$LOCALAPPDATA\\Microsoft\\Windows\\fonts,$WINDIR\\fonts \
         ../*.adoc \
